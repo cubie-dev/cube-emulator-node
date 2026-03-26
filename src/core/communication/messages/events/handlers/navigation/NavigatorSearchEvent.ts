f2493042fb82a4b60920eb3cc356194c0071f053
eran <dev@eranmachiels.nl>
2026-03-26 23:31:01 +0100

    Basic navigator

--- src/core/communication/messages/events/handlers/navigation/NavigatorSearchEvent.ts
+++ src/core/communication/messages/events/handlers/navigation/NavigatorSearchEvent.ts
@@ -10,14 +10,58 @@ export class NavigatorSearchEvent extends EventHandler {
         const navigatorContextCode = eventContext.event.reader.readString();
         const query = eventContext.event.reader.readString();
 
+        //The frontend can handle multiple result categories.
+        // Except for our own world, we will group rooms by their category
+
         // requesting all rooms in the view
-        const rooms = await eventContext.em
-            .getRepository(Room)
-            .getPublicRooms();
+        const rooms = await this.getRooms(eventContext, navigatorContextCode, query);
+
+        const roomsPerCategory = rooms.reduce<Record<string, Room[]>>((acc, room) => {
+            const category = room.category.name;
+
+            if (!acc[category]) {
+                acc[category] = [];
+            }
+
+            acc[category].push(room);
+
+            return acc;
+        }, {});
 
         return new NavigatorSearchResponse(
             navigatorContextCode,
-            rooms,
+            roomsPerCategory,
         );
+    }
+
+    private async getRooms(eventContext: EventContext, context: string, query: string): Promise<Room[]> {
+        if (context === 'official_view') {
+            return eventContext.em
+                .getRepository(Room)
+                .find(
+                    { owner: null },
+                    { populate: ['category', 'owner'] }
+                )
+        }
+
+        if (context === 'hotel_view') {
+            return eventContext.em
+                .getRepository(Room)
+                .find(
+                    { owner: { $exists: true } },
+                    { populate: ['category', 'owner'] }
+                );
+        }
+
+        if (context === 'myworld_view') {
+            return eventContext.em
+                .getRepository(Room)
+                .find(
+                    { owner: eventContext.client.user.id },
+                    { populate: ['category', 'owner'] }
+                );
+        }
+
+        return [];
     }
 }

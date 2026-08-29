5e9dd5ba9db14dd5c9e5d2ee225b167981bbee01
EranNL <dev@eranmachiels.nl>
2026-08-29 10:02:48 +0200

    Removed unused stuff

--- src/core/database/repositories/RoomRepository.ts
+++ src/core/database/repositories/RoomRepository.ts
@@ -2,11 +2,4 @@ import { EntityRepository, type FilterQuery } from '@mikro-orm/core';
 import { Room } from '../entities/Room';
 
 export class RoomRepository extends EntityRepository<Room> {
-    public async getPublicRoomsByCategory(): Promise<Room[]> {
-        return this.find({
-            owner: null,
-        }, {
-            populate: ['category']
-        });
-    }
 }
